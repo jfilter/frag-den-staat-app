0a5700f3e4c872618ad29e7239a31990cc39a52d
Johannes Filter <hi@jfilter.de>
2017-06-27 20:38:18 +0200

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,29 @@
 module.exports = {
-    "parser": "babel-eslint",
-    "extends": "airbnb",
-    "plugins": [
-        "react",
-        "react-native",
-        "jsx-a11y",
-        "import"
+  parser: 'babel-eslint',
+  extends: ['airbnb', 'prettier'],
+  plugins: ['react', 'react-native', 'jsx-a11y', 'import', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 6,
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: 'module',
+  },
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'no-underscore-dangle': 'off',
+    'arrow-body-style': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'es5',
+      },
     ],
-    "parserOptions": {
-        ecmaVersion: 6,
-        ecmaFeatures: {
-            jsx: true,
-        },
-        sourceType: "module"
-    },
-    "rules": {
-        "react/jsx-filename-extension": "off",
-        "no-underscore-dangle": "off",
-        "arrow-body-style": "off",
-    },
-    "globals": {
-        "fetch": false,
-        "require": false,
-        "navigator": false,
-    }
+  },
+  globals: {
+    fetch: false,
+    require: false,
+    navigator: false,
+  },
 };
