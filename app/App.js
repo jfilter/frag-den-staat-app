07bccd3640566d98025c8e97f451d56b853c871c
Johannes Filter <hi@jfilter.de>
2017-06-27 16:12:39 +0200

    add redux-thunk and redux-logger

--- app/App.js
+++ app/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import AppReducer from './reducers';
 import AppWithNavigationState from './navigators/AppNavigator';
 
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+
+// TODO: retrieve state from persistent storage for e.g. auth
+const initialState = {};
+
 class App extends React.Component {
-  store = createStore(AppReducer);
+  loggerMiddleware = createLogger();
+  store = createStore(
+    AppReducer,
+    initialState,
+    applyMiddleware(thunkMiddleware, this.loggerMiddleware));
 
   render() {
     return (
