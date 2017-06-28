53b45ecb7671c9a066a7eb3d47fadde0b2a4c0d3
Johannes Filter <hi@jfilter.de>
2017-06-28 12:24:11 +0200

    add first basic version of  ListView for requests

--- app/containers/FoiaRequestsListScreen.js
+++ app/containers/FoiaRequestsListScreen.js
@@ -1,18 +1,55 @@
 import React from 'react';
-import { Button, Text, View } from 'react-native';
+import { FlatList, Text, View, StyleSheet, Image } from 'react-native';
+import { ListItem } from 'react-native-elements';
 
 import { connect } from 'react-redux';
 import { foiaRequestsFetchData } from '../actions/foiaRequests';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
+
 class FoiaRequestsListScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this._renderItem = this._renderItem.bind(this);
+    // this._loadMoreData = this._loadMoreData.bind(this);
+  }
+
   componentDidMount() {
     this.props.fetchData();
   }
 
+  _renderItem = ({ item, index }) => {
+    const imagePath = `${item.status}`;
+    const lastContact = item.last_message || item.first_message;
+    const subtitle = `${item.status},\n ${lastContact},\n${item.public_body}`;
+
+    return (
+      <ListItem
+        key={item.id}
+        title={item.title}
+        titleNumberOfLines={3}
+        subtitle={subtitle}
+        subtitleNumberOfLines={3}
+        avatar={{
+          uri: imagePath,
+        }}
+        // avatarStyle={{ marginTop: 20 }}
+        // TODO: Not possible right now, come back later to check if they have fixed it.
+        // avatarStyle={{ overlayContainerStyle: { backgroundColor: 'white' } }}
+        containerStyle={{ alignItems: 'center', justifyContent: 'center' }}
+      />
+    );
+  };
+
   render() {
-    console.log(this.props);
     if (this.props.error !== '') {
-      console.log(this.props.error);
       return (
         <View>
           <Text>ERROR!</Text>
@@ -29,11 +66,7 @@ class FoiaRequestsListScreen extends React.Component {
 
     return (
       <View>
-        {this.props.requests.map(request =>
-          <Text key={request.id}>
-            {request.id}
-          </Text>
-        )}
+        <FlatList data={this.props.requests} renderItem={this._renderItem} />
       </View>
     );
   }
