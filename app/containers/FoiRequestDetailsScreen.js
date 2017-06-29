1034ef8dab9cdea0bee0f14a324e6f5966f7394d
Johannes Filter <hi@jfilter.de>
2017-06-29 14:55:52 +0200

    add share button to details screen

--- app/containers/FoiRequestDetailsScreen.js
+++ app/containers/FoiRequestDetailsScreen.js
@@ -7,10 +7,16 @@ import {
   ActivityIndicator,
 } from 'react-native';
 import moment from 'moment';
+import { Share } from 'react-native';
 import { connect } from 'react-redux';
 import { NavigationActions } from 'react-navigation';
+import { Icon } from 'react-native-elements';
 
 import publicBodyFile from '../../scraper/public_bodies/public_bodies_cleaned.json';
+
+// import Icon from 'react-native-vector-icons/Ionicons';
+
+import { primaryColor } from '../styles/colors.js';
 
 const styles = StyleSheet.create({
   container: {
@@ -22,10 +28,39 @@ const styles = StyleSheet.create({
 });
 
 class FoiRequestDetailsScreen extends React.Component {
-  static navigationOptions = ({ navigation, screenProps }) => ({
-    title: `Request #${navigation.state.params.id}`,
-    // headerRight: <Button color={screenProps.tintColor} {...} />,
-  });
+  static navigationOptions = ({ navigation, screenProps }) => {
+    const requestId = navigation.state.params.id;
+    function share() {
+      Share.share(
+        {
+          message: 'LOL WAT? Check out this FOI request!',
+          url: `https://fragdenstaat.de/a/${requestId}`,
+          title: 'FragDenStaat.de', // What's the purpose?
+        },
+        {
+          // Android only:
+          dialogTitle: 'FragDenStaat.de',
+        }
+      );
+    }
+
+    return {
+      title: `Request #${requestId}`,
+      headerRight: (
+        <Icon
+          name="ios-share-outline"
+          type="ionicon"
+          color={primaryColor}
+          size={30}
+          onPress={share}
+          containerStyle={{
+            paddingVertical: 7,
+            paddingHorizontal: 20,
+          }}
+        />
+      ),
+    };
+  };
 
   render() {
     return (
