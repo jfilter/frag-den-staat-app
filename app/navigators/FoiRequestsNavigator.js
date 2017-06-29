7eeec007a866c812ac248ae5262bfb996fb2bbc9
Johannes Filter <hi@jfilter.de>
2017-06-29 14:55:33 +0200

    change header colors of stack navigator to primary color

--- app/navigators/FoiRequestsNavigator.js
+++ app/navigators/FoiRequestsNavigator.js
@@ -4,6 +4,7 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import FoiRequestsListScreen from '../containers/FoiRequestsListScreen';
 import FoiRequestDetailsScreen from '../containers/FoiRequestDetailsScreen';
+import { primaryColor } from '../styles/colors';
 
 const FoiRequestsNavigator = StackNavigator(
   {
@@ -12,6 +13,8 @@ const FoiRequestsNavigator = StackNavigator(
   },
   {
     navigationOptions: {
+      headerTintColor: primaryColor,
+      headerTitleStyle: { color: 'black' }, // change the color back to black from the overriden primary color
       tabBarLabel: 'Requests',
       tabBarIcon: () => <Icon size={24} color="black" name="list" />,
     },
