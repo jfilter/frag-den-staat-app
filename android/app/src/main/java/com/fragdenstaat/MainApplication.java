ade9a1c3ddd0de8e3d1de3027f3e51b4079b1756
Johannes Filter <hi@jfilter.de>
2017-07-03 00:06:57 +0200

    add pdf viewer

--- android/app/src/main/java/com/fragdenstaat/MainApplication.java
+++ android/app/src/main/java/com/fragdenstaat/MainApplication.java
@@ -3,6 +3,8 @@ package com.fragdenstaat;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.RNFetchBlob.RNFetchBlobPackage;
+import org.wonday.pdf.RCTPdfView;
 import com.oblador.vectoricons.VectorIconsPackage;
 import io.realm.react.RealmReactPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFetchBlobPackage(),
+            new RCTPdfView(),
             new VectorIconsPackage(),
             new RealmReactPackage()
       );
