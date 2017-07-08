f191ae9ac9add7ed56070668525f4e837d253ab0
Johannes Filter <hi@jfilter.de>
2017-07-08 20:38:34 +0200

    add Hockey App to distribute Android beta apps

--- android/app/src/main/java/de/fragdenstaat/app/MainApplication.java
+++ android/app/src/main/java/de/fragdenstaat/app/MainApplication.java
@@ -12,6 +12,9 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.slowpath.hockeyapp.RNHockeyAppModule;
+import com.slowpath.hockeyapp.RNHockeyAppPackage;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -26,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
+          new RNHockeyAppPackage(MainApplication.this),
           new MainReactPackage(),
             new RNFetchBlobPackage(),
             new RCTPdfView(),
--- index.android.js
+++ index.android.js
@@ -1,4 +1,22 @@
 import { AppRegistry } from 'react-native';
-import App from './app/App';
+import React, { Component } from 'react';
+import HockeyApp from 'react-native-hockeyapp';
 
-AppRegistry.registerComponent('FragDenStaat', () => App);
+import App from './app/App';
+import { HockeyAppId } from './secrets.json';
+
+class AndroidApp extends Component {
+  componentWillMount() {
+    HockeyApp.configure(HockeyAppId, true);
+  }
+
+  componentDidMount() {
+    HockeyApp.start();
+    HockeyApp.checkForUpdate();
+  }
+  render() {
+    return <App />;
+  }
+}
+
+AppRegistry.registerComponent('FragDenStaat', () => AndroidApp);
