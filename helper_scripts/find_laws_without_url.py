7c0af6e800a5e4f25a1d261541009175dca6d100
Johannes Filter <hi@jfilter.de>
2017-12-17 00:34:54 +0100

    remove old helper scripts, add new one, change location

--- helper_scripts/find_laws_without_url.py
+++ helper_scripts/find_laws_without_url.py
@@ -1,24 +1,25 @@
 import requests
 import json
 
-base_url = 'https://fragdenstaat.de'
-next_url = '/api/v1/law/'
+next_url = 'https://fragdenstaat.de/api/v1/law/'
 data = []
 
 while(next_url):
-    url = base_url + next_url
+    url = next_url
     r = requests.get(url)
     j = json.loads(r.content)
     objects = j['objects']
     for o in objects:
         d = {}
         d['name'] = o['name']
-        d['site_url'] = o['site_url']
-        d['resource_uri'] = o['resource_uri']
-        data.append(d)
+        d['url'] = o['url']
+        d['id'] = o['id']
+
+        if (o['url'] == ''):
+            data.append(d)
 
     next_url = j['meta']['next']
     print("next_url", next_url)
 
-with open('../../app/data/laws.json', 'w') as outfile:
+with open('laws_no_url.json', 'w') as outfile:
     json.dump(data, outfile)
