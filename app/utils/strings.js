2927391282cf1428b35961fb81c78218656b0bef
Johannes Filter <hi@jfilter.de>
2017-12-27 14:24:14 +0100

    hotfix to remove word breaking

--- app/utils/strings.js
+++ app/utils/strings.js
@@ -1,12 +1,14 @@
 function breakLongWords(string) {
+  // Hotfix, not really helpful fo rnow.
+  return string;
   const breakWord = 'gesetz';
   const words = string.split(' ');
   return words
     .map(
-      x =>
-        x.length > 10 && x.includes(breakWord)
-          ? x.replace(breakWord, `- ${breakWord}`)
-          : x
+    x =>
+      x.length > 10 && x.includes(breakWord)
+        ? x.replace(breakWord, `- ${breakWord}`)
+        : x
     )
     .join(' ');
 }
