9a78dd71786fdd703ffb4ce3ecf9aa50f05f705c
Johannes Filter <hi@jfilter.de>
2018-01-28 00:41:42 +0100

    refactor list renderer into proper components #wip 🚧

---
+++ app/components/library/Seperator/index.js
@@ -0,0 +1,10 @@
+import { View } from 'react-native';
+import React from 'react';
+
+import styles from './styles';
+
+const Seperator = () => {
+  return <View style={styles.seperator} />;
+};
+
+export default Seperator;
---
+++ app/components/library/Seperator/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+import { greyLight } from '../../../globals/colors';
+
+export default StyleSheet.create({
+  seperator: {
+    height: 1,
+    backgroundColor: greyLight,
+  },
+});
