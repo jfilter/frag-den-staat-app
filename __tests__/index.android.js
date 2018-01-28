e06dd35ccbc5ce20c2a2b79901a6ece197b990a7
Johannes Filter <hi@jfilter.de>
2018-01-28 20:17:26 +0100

    add test whether it renders or not

--- __tests__/index.android.js
+++ __tests__/index.android.js
@@ -5,8 +5,19 @@ import Index from '../index.android.js';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+jest.mock('react-native-fetch-blob', () => {
+  return {
+    DocumentDir: () => {},
+    polyfill: () => {},
+  };
+});
+
+jest.mock('ScrollView', () => jest.genMockFromModule('ScrollView'));
+
+jest.mock('realm', () => {
+  return require('../mocks/realm');
+});
+
 it('renders correctly', () => {
-  const tree = renderer.create(
-    <Index />
-  );
+  const tree = renderer.create(<Index />);
 });
