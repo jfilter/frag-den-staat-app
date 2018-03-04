f0362873fd70adb6e6832b1d79a0ad8bb90598ef
Johannes Filter <hi@jfilter.de>
2018-03-04 19:26:23 +0100

    fix typo

--- app/globals/index.js
+++ app/globals/index.js
@@ -2,7 +2,7 @@ const ORIGIN = 'https://fragdenstaat.de';
 
 const FOI_REQUESTS_PAGE_SIZE = 20;
 
-const FOI_REQEST_PATH = '/api/v1/request/';
+const FOI_REQUESTS_PATH = '/api/v1/request/';
 const PUBLIC_BODIES_PATH = '/api/v1/publicbody/';
 const SEARCH_FOI_REQUESTS_PATH = '/api/v1/request/search/';
 const SEARCH_PUBLIC_BODIES_PATH = '/api/v1/publicbody/search/';
@@ -10,7 +10,7 @@ const SEARCH_PUBLIC_BODIES_PATH = '/api/v1/publicbody/search/';
 export {
   ORIGIN,
   FOI_REQUESTS_PAGE_SIZE,
-  FOI_REQEST_PATH,
+  FOI_REQUESTS_PATH,
   PUBLIC_BODIES_PATH,
   SEARCH_FOI_REQUESTS_PATH,
   SEARCH_PUBLIC_BODIES_PATH,
