bdfb97f8fedcff8d780d37305d4ef15257434896
Johannes Filter <hi@jfilter.de>
2018-03-07 23:45:57 +0100

    add ptional text prop to nav bar icon

--- src/components/foiRequests/NavBarIcon/index.js
+++ src/components/foiRequests/NavBarIcon/index.js
@@ -1,22 +1,34 @@
 import { Icon } from 'react-native-elements';
+import { TouchableHighlight, View, Text } from 'react-native';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import { primaryColor } from '../../../globals/colors';
+import { primaryColor, greyLight } from '../../../globals/colors';
 
-const NavBarIcon = ({ onPress, iconName, iconType }) => (
-  <Icon
+const NavBarIcon = ({ onPress, iconName, iconType, text }) => (
+  <TouchableHighlight
     hitSlop={{ top: 20, bottom: 10, left: 20, right: 20 }}
-    name={iconName}
-    type={iconType}
-    color={primaryColor}
-    size={26}
     onPress={onPress}
-    containerStyle={{
-      paddingVertical: 7,
-      paddingHorizontal: 10,
-    }}
-  />
+    underlayColor={greyLight}
+  >
+    <View
+      style={{
+        marginVertical: 3,
+        marginHorizontal: 5,
+        paddingVertical: 2,
+        paddingHorizontal: 5,
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderColor: primaryColor,
+        borderWidth: text == null ? 0 : 1,
+        borderRadius: 2,
+      }}
+    >
+      <Icon name={iconName} type={iconType} color={primaryColor} size={26} />
+      <Text style={{ color: primaryColor, paddingLeft: 4 }}>{text}</Text>
+    </View>
+  </TouchableHighlight>
 );
 
 NavBarIcon.propTypes = {
