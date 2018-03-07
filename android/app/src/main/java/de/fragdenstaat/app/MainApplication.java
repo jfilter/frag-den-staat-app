9520f86d23906659cf6e873cc77c22b35a02ae3d
Johannes Filter <hi@jfilter.de>
2018-03-07 17:37:23 +0100

    add storage for sensitive data w/ react-native-keychain

--- android/app/src/main/java/de/fragdenstaat/app/MainApplication.java
+++ android/app/src/main/java/de/fragdenstaat/app/MainApplication.java
@@ -3,6 +3,7 @@ package de.fragdenstaat.app;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.keychain.KeychainPackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import io.realm.react.RealmReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new KeychainPackage(),
             new RNI18nPackage(),
             new RealmReactPackage(),
             new VectorIconsPackage(),
---
+++ src/utils/secureStorage.js
@@ -0,0 +1,24 @@
+import * as Keychain from 'react-native-keychain';
+
+const saveToken = async token => {
+  const username = 'token';
+  const password = JSON.stringify(token);
+
+  // Store the credentials
+  await Keychain.setGenericPassword(username, password);
+};
+
+const loadToken = async () => {
+  try {
+    // Retreive the credentials
+    const credentials = Keychain.getGenericPassword();
+    if (credentials) {
+      return JSON.parse(credentials.password);
+    }
+  } catch (error) {
+    console.log("Keychain couldn't be accessed!", error);
+  }
+  return null;
+};
+
+export { saveToken, loadToken };
