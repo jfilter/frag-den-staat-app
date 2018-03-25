cf56506d5c3e280257e79779aede0c01c144275b
Johannes Filter <hi@jfilter.de>
2018-03-25 19:53:25 +0200

    add share button for request web view

--- src/components/foiRequests/WebView/index.js
+++ src/components/foiRequests/WebView/index.js
@@ -1,14 +1,56 @@
-import { WebView } from 'react-native';
+import { Platform, Share, WebView } from 'react-native';
 import React from 'react';
+
+import NavBarIcon from '../NavBarIcon';
 
 const FoiRequestsWebView = ({ navigation }) => (
   <WebView source={{ uri: navigation.state.params.uri }} />
 );
 
-FoiRequestsWebView.navigationOptions = {
-  title: 'Browser-Ansicht',
-  tabBarVisible: false,
-  drawerLockMode: 'locked-closed', // disable global drawer
+FoiRequestsWebView.navigationOptions = props => {
+  const url = props.navigation.state.params.uri;
+
+  function share() {
+    Share.share(
+      {
+        ...Platform.select({
+          ios: {
+            url,
+          },
+          android: {
+            message: url,
+          },
+        }),
+        title: 'FragDenStaat',
+      },
+      {
+        ...Platform.select({
+          android: {
+            // Android only:
+            dialogTitle: `Share: ${url}`,
+          },
+        }),
+      }
+    );
+  }
+
+  let iconName = 'share';
+  let iconType = 'material';
+
+  // platform specific share button
+  if (Platform.OS === 'ios') {
+    iconName = 'ios-share-outline';
+    iconType = 'ionicon';
+  }
+
+  return {
+    drawerLockMode: 'locked-closed', // disable global drawer
+    title: 'Browser-Ansicht',
+    tabBarVisible: false,
+    headerRight: (
+      <NavBarIcon iconName={iconName} iconType={iconType} onPress={share} />
+    ),
+  };
 };
 
 export default FoiRequestsWebView;
