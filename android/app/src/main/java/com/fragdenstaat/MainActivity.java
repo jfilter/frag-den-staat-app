7857529f5ed951d78c8404188e7d47b0e07de2d9
Johannes Filter <hi@jfilter.de>
2019-06-07 15:08:06 +0200

    fix android gestures

--- android/app/src/main/java/com/fragdenstaat/MainActivity.java
+++ android/app/src/main/java/com/fragdenstaat/MainActivity.java
@@ -2,6 +2,12 @@ package com.fragdenstaat;
 
 import com.facebook.react.ReactActivity;
 
+// https://kmagiera.github.io/react-native-gesture-handler/docs/getting-started.html
+
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
 public class MainActivity extends ReactActivity {
 
     /**
@@ -12,4 +18,14 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "FragDenStaat";
     }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+       return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
+  }
 }
