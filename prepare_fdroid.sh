06da892964ae3661393e07d156503f81641b8edb
Johannes Filter <hi@jfilter.de>
2019-06-07 23:46:13 +0200

    adapt fdroid script to different sed version

--- prepare_fdroid.sh
+++ prepare_fdroid.sh
@@ -4,5 +4,7 @@ set -x
 
 # 1. hide alert screen
 # 2. unlink react-native-push-notification
-sed -i '' 's/const FDROID = false;/const FDROID = true;/' src/globals/index.js &&
+# different sed versions for mac and linux
+# sed -i '' 's/const FDROID = false;/const FDROID = true;/' src/globals/index.js &&
+sed -i s/const FDROID = false;/const FDROID = true;/' src/globals/index.js &&
 react-native unlink react-native-push-notification
