571ea52bcbf0b8e2b23338cc85359b197d53e7d7
Johannes Filter <hi@jfilter.de>
2019-06-08 10:54:34 +0200

    fix fdroid script

--- prepare_fdroid.sh
+++ prepare_fdroid.sh
@@ -6,5 +6,6 @@ set -x
 # 2. unlink react-native-push-notification
 # different sed versions for mac and linux
 # sed -i '' 's/const FDROID = false;/const FDROID = true;/' src/globals/index.js &&
-sed -i s/const FDROID = false;/const FDROID = true;/' src/globals/index.js &&
-react-native unlink react-native-push-notification
+sed -i 's/const FDROID = false;/const FDROID = true;/' src/globals/index.js &&
+react-native unlink react-native-push-notification &&
+npm uninstall react-native-push-notification
