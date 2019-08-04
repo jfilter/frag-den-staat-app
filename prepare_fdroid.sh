836a340bb5d8a154ad73c4b1b52d4a0f907f46f3
Johannes Filter <hi@jfilter.de>
2019-08-04 14:28:05 +0200

    trying to fix fdroid

--- prepare_fdroid.sh
+++ prepare_fdroid.sh
@@ -7,11 +7,10 @@ set -x
 # 2. remove reference to react-native-push-notification
 # 3. unlink react-native-push-notification
 # 4. fully remove the package
+# 5. some fixes
 
-# NB: different sed versions for mac and linux, the preceding '' is required for mac
-# sed -i '' 's/const FDROID = false;/const FDROID = true;/' src/globals/index.js &&
-# sed -i '' "s/import PushNotification from 'react-native-push-notification';/ /" src/utils/notifications.js &&
 sed -i 's/const FDROID = false;/const FDROID = true;/' src/globals/index.js &&
 sed -i "s/import PushNotification from 'react-native-push-notification';/let PushNotification = null;/" src/utils/notifications.js &&
 react-native unlink react-native-push-notification &&
-yarn remove react-native-push-notification
+yarn remove react-native-push-notification &&
+npm i jetifier && npx jetify
